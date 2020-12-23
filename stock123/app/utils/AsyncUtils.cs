073ed1b15abf9a147a17b913c5f6dfec915c448e
DESKTOP-2BQ6PUE\LongVT <thuyps@yahoo.com>
2020-12-23 10:48:18 +0700

    1

---
+++ stock123/app/utils/AsyncUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+
+namespace stock123.app.utils
+{
+    static class AsyncUtils
+    {
+        static public void DelayCall(int msec, Action fn)
+        {
+            // Grab the dispatcher from the current executing thread
+            Dispatcher d = Dispatcher.CurrentDispatcher;
+
+            // Tasks execute in a thread pool thread
+            new Task(() =>
+            {
+                System.Threading.Thread.Sleep(msec);   // delay
+
+                // use the dispatcher to asynchronously invoke the action 
+                // back on the original thread
+                d.BeginInvoke(fn);
+            }).Start();
+        }
+    }
+}
